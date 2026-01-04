6fe7e799429262e2de7aae3677d72f0a723ceb2a
akarauser <252939133+akarauser@users.noreply.github.com>
2026-01-04 23:20:54 +0200

    Add files via upload

---
+++ shelf_wise/utils/_validation.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from pydantic import BaseModel
+
+from ._logger import logger
+
+
+class ConfigValidation(BaseModel):
+    data_path: str
+    num_epochs: int
+    batch_size: int
+    learning_rate: float
+    embedding_size: int
+
+
+def _read_json_file() -> Any | None:
+    config_path = Path(f"{Path(__file__).parent}\\config.json")
+    try:
+        with config_path.open(encoding="utf-8") as file:
+            config_values = json.load(file)
+            return config_values
+    except FileNotFoundError:
+        logger.error("Config file not found.")
+
+
+try:
+    config_args = ConfigValidation.model_validate(_read_json_file())
+except ValueError:
+    logger.error("Validation failed.")
